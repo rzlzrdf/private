334915b856d3c9a7665e72d355a218229f8763e8
Rizal Lazuardi Firdaus <93332704+rzlzrdf@users.noreply.github.com>
2022-11-21 15:29:47 +0700

    .

--- src/Pages/Services/Services.js
+++ src/Pages/Services/Services.js
@@ -1,6 +1,5 @@
 import React from 'react'
-import { Card, Col, Container, Row } from 'react-bootstrap'
-// import {  Col, Container, Row } from 'react-bootstrap'
+import {  Col, Container, Row } from 'react-bootstrap'
 import Header from './Header'
 import style from './Services.module.css'
 
